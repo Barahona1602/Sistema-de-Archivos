c421aa736ebdc264dbe84955091e423a52b06217
Pablo Barahona <pbarahona07@gmail.com>
2023-02-20 23:55:41 -0600

    reportes casi finalizados

--- EDD_Proyecto1_Fase1/main.go
+++ EDD_Proyecto1_Fase1/main.go
@@ -5,7 +5,9 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/sqweek/dialog"
 )
@@ -28,6 +30,7 @@ type DoublyLinkedList struct {
 	tail *Node
 }
 
+// Colas de estudiantes
 type Queue []User
 
 func (q *Queue) Enqueue(user User) {
@@ -47,6 +50,226 @@ func (dll *DoublyLinkedList) Insert(user User) {
 	}
 }
 
+// Pila de aprobar o rechazar estudiantes
+
+type Stack2 []string
+
+func (s2 *Stack2) Pop() string {
+	if len(*s2) == 0 {
+		return ""
+	}
+	index := len(*s2) - 1
+	element := (*s2)[index]
+	*s2 = (*s2)[:index]
+	return element
+}
+
+func (s2 *Stack2) Push(startTime string) {
+	*s2 = append(*s2, startTime)
+	generateGraph2(*s2)
+}
+
+// Pila de inicio de sesión
+type Stack []string
+
+func (s *Stack) Pop() string {
+	if len(*s) == 0 {
+		return ""
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element
+}
+
+func (s *Stack) Push(nombre string, apellido string, hora string, carnet string) {
+	usuario := carnet + "\n" + nombre + " " + apellido
+	estaRepetido := false
+	for i, v := range *s {
+		if strings.Contains(v, usuario) {
+			(*s)[i] = v + "\n" + hora
+			estaRepetido = true
+			break
+		}
+	}
+	if !estaRepetido {
+		*s = append(*s, usuario+"\n"+"Se inició sesión:"+"\n"+hora)
+	}
+	generateGraph(*s)
+}
+
+// Graphviz para pila de inicio de sesión
+func generateGraph(s Stack) {
+	f, err := os.Create("estudiantes.dot")
+	if err != nil {
+		fmt.Println("Error al crear archivo de grafo:", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString("digraph {\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+
+	// Escribir nodos
+	_, err = f.WriteString("node [shape=box]\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	_, err = f.WriteString("{rank=same ")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	for i, v := range s {
+		_, err = f.WriteString(fmt.Sprintf("\"%s\"", v))
+		if err != nil {
+			fmt.Println("Error al escribir en archivo de grafo:", err)
+			return
+		}
+		if i < len(s)-1 {
+			_, err = f.WriteString(" -> ")
+			if err != nil {
+				fmt.Println("Error al escribir en archivo de grafo:", err)
+				return
+			}
+		}
+	}
+	_, err = f.WriteString("}\n")
+
+	// Escribir conexiones
+	_, err = f.WriteString("edge [dir=none]\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	_, err = f.WriteString("{rank=same ")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	for i, v := range s {
+		_, err = f.WriteString(fmt.Sprintf("\"%s\"", v))
+		if err != nil {
+			fmt.Println("Error al escribir en archivo de grafo:", err)
+			return
+		}
+		if i < len(s)-1 {
+			_, err = f.WriteString(" -> ")
+			if err != nil {
+				fmt.Println("Error al escribir en archivo de grafo:", err)
+				return
+			}
+		}
+	}
+	_, err = f.WriteString("}\n")
+
+	_, err = f.WriteString("}\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+
+	cmd := exec.Command("dot", "-Tpng", "estudiantes.dot", "-o", "estudiantes.png")
+	if err = cmd.Run(); err != nil {
+		fmt.Println("Error al generar imagen de grafo:", err)
+		return
+	}
+}
+
+// Graphviz para pila de inicio de sesión
+func generateGraph2(s2 Stack2) {
+	f, err := os.Create("admin.dot")
+	if err != nil {
+		fmt.Println("Error al crear archivo de grafo:", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString("digraph {\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+
+	// Escribir configuración de grafo
+	_, err = f.WriteString("rankdir=LR;\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+
+	// Escribir nodos
+	_, err = f.WriteString("node [shape=box]\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	_, err = f.WriteString("{rank=same ")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	for i, v := range s2 {
+		_, err = f.WriteString(fmt.Sprintf("\"%s\"", v))
+		if err != nil {
+			fmt.Println("Error al escribir en archivo de grafo:", err)
+			return
+		}
+		if i < len(s)-1 {
+			_, err = f.WriteString(" -> ")
+			if err != nil {
+				fmt.Println("Error al escribir en archivo de grafo:", err)
+				return
+			}
+		}
+	}
+	_, err = f.WriteString("}\n")
+
+	// Escribir conexiones
+	_, err = f.WriteString("edge [dir=none]\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	_, err = f.WriteString("{rank=same ")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+	for i, v := range s2 {
+		_, err = f.WriteString(fmt.Sprintf("\"%s\"", v))
+		if err != nil {
+			fmt.Println("Error al escribir en archivo de grafo:", err)
+			return
+		}
+		if i < len(s2)-1 {
+			_, err = f.WriteString(" -> ")
+			if err != nil {
+				fmt.Println("Error al escribir en archivo de grafo:", err)
+				return
+			}
+		}
+	}
+	_, err = f.WriteString("}\n")
+
+	_, err = f.WriteString("}\n")
+	if err != nil {
+		fmt.Println("Error al escribir en archivo de grafo:", err)
+		return
+	}
+
+	cmd := exec.Command("dot", "-Tpng", "admin.dot", "-o", "admin.png")
+	if err = cmd.Run(); err != nil {
+		fmt.Println("Error al generar imagen de grafo:", err)
+		return
+	}
+}
+
+// Función para ordenar lista por carnet
 func (dll *DoublyLinkedList) OrdenarPorCarnet() {
 	for {
 		permutacion := false
@@ -64,6 +287,7 @@ func (dll *DoublyLinkedList) OrdenarPorCarnet() {
 	}
 }
 
+// Función para ordenar lista por nombre
 func (dll *DoublyLinkedList) BuscarUsuario(carnet string) *Node {
 	for node := dll.head; node != nil; node = node.next {
 		if node.user.carnet == carnet {
@@ -73,11 +297,23 @@ func (dll *DoublyLinkedList) BuscarUsuario(carnet string) *Node {
 	return nil
 }
 
+// Standby
+func standby() {
+	fmt.Println("Gracias por usar el programa")
+	var choice int
+	fmt.Scan(&choice)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+}
+
 func main() {
 	menuInicioSesion(&DoublyLinkedList{})
 	admin()
 }
 
+var s = Stack{}
+
+// Función para iniciar sesión
 func menuInicioSesion(dll *DoublyLinkedList) {
 	var carnet, contrasenia string
 	var userNode *Node
@@ -114,6 +350,12 @@ func menuInicioSesion(dll *DoublyLinkedList) {
 				continue
 			}
 			if contrasenia == "admin" {
+				fmt.Println("Bienvenido admin")
+				now := time.Now()
+				var hora string = now.Format("02/01/2006 15:04:05")
+				s.Push("admin", "", hora, carnet)
+
+				fmt.Println("La pila es:", s)
 				admin()
 				continue
 			} else {
@@ -137,14 +379,34 @@ func menuInicioSesion(dll *DoublyLinkedList) {
 			continue
 		} else {
 			fmt.Printf("Bienvenido, %s %s.\n", userNode.user.nombre, userNode.user.apellido)
-			return
+			now := time.Now()
+			var hora string = now.Format("02/01/2006 15:04:05")
+
+			s.Push(userNode.user.nombre, userNode.user.apellido, hora, carnet)
+
+			fmt.Println("La pila es:", s)
+
+			fmt.Print("¿Desea salir? (y/n)")
+			var opcion2 string
+			fmt.Scan(&opcion2)
+			if opcion2 == "y" {
+				menuInicioSesion(dll)
+			}
+			if opcion2 == "n" {
+				standby()
+			}
 		}
 	}
 }
 
+var doublyLinkedList = DoublyLinkedList{}
+var queue = Queue{}
+
+var s2 Stack2
+
+// Función para administrar usuarios
 func admin() {
-	doublyLinkedList := DoublyLinkedList{}
-	queue := Queue{}
+
 	var nombre, apellido, carnet, contraseña string
 	for {
 		fmt.Println("* * * * * * * Administración de usuarios * * * * * * *")
@@ -195,6 +457,9 @@ func admin() {
 					// Eliminar el usuario seleccionado de la cola
 					copy(queue[index-1:], queue[index:])
 					queue = queue[:len(queue)-1]
+					now2 := time.Now()
+					var hora string = now2.Format("02/01/2006 15:04:05")
+					s2.Push("Se aprobó a Estudiante" + "\n" + hora)
 				}
 
 			}
